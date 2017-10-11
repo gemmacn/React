646a5df8150e7b96e6b2e1fe20529226ff344742
GEMMA CASANOVAS <GEMMA CASANOVAS>
2017-10-11 14:54:05 +0200

    con apuntes

--- TODO proyecto/main.jsx
+++ TODO proyecto/main.jsx
@@ -5,25 +5,25 @@ class App extends React.Component {
 		super()
 
 		this.state={
-			items: []
+			items: []// tengo que guardar un listado de tareas
 		}
 	}
 
 				handleEnterPress =(e) => {
-					if (e.key.toLowerCase() === 'enter') {
-						const text = e.target.value
-
-						if (!text.length)
+					if (e.key.toLowerCase() === 'enter') {//Poso en lowercase la paraula Enter que per defecte em sortirá pq no hi hagiin problemes entre navegadors
+						const text = e.target.value// el valor del lugar donde ha ocurrido el evento ( input)
+						e.target.value=' '//limpio el input cada vez
+						if (!text.length)// me aseguro q el texto tenga valor( que haya texto)
 			           		return;
 
 				        const item = {
 				            id: Date.now(),// aixi em genera com un id dinámic
-				            text: text,
-				            done: false
+				            text: text, //el texto de cada tarea ya viene dado por el e.target.value
+				            done: false // se cambiará a true cuando la tarea este hecha
 				        }
 
-				        this.setState(function(prevState) {
-				            return {
+				        this.setState(function(prevState) {// Setstate provoca un nuevo render, un cambio de estado en el componente.
+				            return {// el return tiene que darme el nuevo estado, acumulando en este caso el estado anterior.
 				                items: prevState.items.concat(item)
 				            }
 				        })
@@ -31,10 +31,26 @@ class App extends React.Component {
 				}
 handleChange=()=>{
 	var checked=event.target.checked
-	item.done=true
+	item.done=true//(aixó ho he de fer desde un setState i tindré que retornar algo a més)
 	
 	console.log('checked')
 }
+
+/* 
+"Esta sería la funcion que estoy intentando hacer en la handleChange"
+markTaskdone =(event)=>{
+	const id = event.target.value
+
+	this.setState(function(prevState{
+	const tasks = prevState.tasks.map(task =>{
+	if (task.id==id){
+	task.done= true
+	}
+	return { tasks}
+	})
+	}))
+}
+*/
 
 	render(){
 		return(
